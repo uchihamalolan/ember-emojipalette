8ddd5c6a88832b35c636a0d2a7beb700a5a0cdea
uchihamalolan <uchihamalolan@gmail.com>
2019-08-05 15:50:36 +0530

    Added ember-cli-github-pages addon

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   let ENV = {
-    modulePrefix: 'emojipalette',
+    modulePrefix: 'demo',
     environment,
     rootURL: '/',
     locationType: 'auto',
