f8e9c767be6416062a39a0bac83795894790b325
uchihamalolan <uchihamalolan@gmail.com>
2019-08-05 15:36:02 +0530

    styling the demo app

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   let ENV = {
-    modulePrefix: 'dummy',
+    modulePrefix: 'emojipalette',
     environment,
     rootURL: '/',
     locationType: 'auto',
