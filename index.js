1633aec7822cebe0953edd9df927c2ee40086ba0
uchihamalolan <uchihamalolan@gmail.com>
2019-08-06 16:19:17 +0530

    meh

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const join = require('path').join;
 module.exports = {
   name: require('./package').name,
   
-  treeForPublic: function() {
-    const imagesDir = new Funnel(join(this.root, 'vendor/images'), {
-      include: ['*.svg'],
-      destDir: '/assets/images'
-    });
-    return mergeTrees([imagesDir]);
-  },
+  // treeForPublic: function() {
+  //   const imagesDir = new Funnel(join(this.root, 'vendor/images'), {
+  //     include: ['*.svg'],
+  //     destDir: '/assets/images'
+  //   });
+  //   return mergeTrees([imagesDir]);
+  // },
   
   included: function (/* app */) {
     this._super.included.apply(this, arguments);
