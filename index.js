95bb1d028448916b22518cff93c6fbfa040236e3
uchihamalolan <uchihamalolan@gmail.com>
2019-08-06 12:44:06 +0530

    Added ember-cli-github-pages addon

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 'use strict';
 
+const Funnel = require('broccoli-funnel');
+const mergeTrees = require('broccoli-merge-trees');
+const join = require('path').join;
+
 module.exports = {
   name: require('./package').name,
+  
+  treeForPublic: function() {
+    const imagesDir = new Funnel(join(this.root, 'vendor/images'), {
+      include: ['*.svg'],
+      destDir: '/assets/images'
+    });
+    return mergeTrees([imagesDir]);
+  },
+  
   included: function (app) {
     this._super.included.apply(this, arguments);
-    const images = ["activity", "people", "flag", "food", "nature", "object", "travel", "symbol"];
-    for(let image of images) {
-      app.import(`vendor/assets/${image}.png`);
-    }
   }
 };
