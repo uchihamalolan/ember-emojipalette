78b99bdb4856a1849b0a6337844c106b1109bf50
uchihamalolan <uchihamalolan@gmail.com>
2019-08-27 12:26:49 +0530

    close on escape and prune emojidata file

--- addon/components/emoji-palette/component.js
+++ addon/components/emoji-palette/component.js
@@ -6,32 +6,76 @@ import { categoryIcons } from 'ember-emojipalette/utils/category-icons';
 
 export default Component.extend({
   layout,
-  // searchEnabled
-  // emoji
+  closeOnEsc: true,
+
+  // Event Handlers
+  handleKeyDown(e) {
+    let code = e.keyCode || e.which;
+    if (code === 27 && this.get('closeOnEsc')) {
+      this.get('onClose')();
+    }
+  },
+
+  // Lifecycle Hooks
+  init() {
+    this._super(...arguments);
+    this.set(
+      'keyDownHandler',
+      this.get('handleKeyDown').bind(this)
+    );
+  },
+  didInsertElement() {
+    this._super(...arguments);
+    document.addEventListener('keydown', this.get('keyDownHandler'));
+  },
+  willDestroyElement() {
+    this._super(...arguments);
+    document.removeEventListener('keyDown', this.handleKeyDown);
+  },
+
+  // Component Properties
+  emojidex: emojidex,
   emojis: emojidex.emojilist,
   categoryNames: emojidex.getCategoryNames(),
   categorySVG: categoryIcons,
-  isCompleteList: computed.alias('hideCategories'),
+  searchTerm: '',
+  searchPlaceholder: 'search for emoji',
 
-  categories: computed(function() {
+  // Computed Properties
+  isCompleteList: computed.alias('hideCategories'),
+  isSearchResult: computed.notEmpty('searchResults'),
+  currentListType: computed(function() {
+    return this.get('isCompleteList') ? 'all' : 'people' ;
+  }),
+  allowedCategories: computed(function() {
     const excludedCategories = this.get('excludedCategories');
     const categoryList = Object.keys(this.get('categoryNames'));
     return (excludedCategories)
       ? categoryList.filter(category => !this.excludedCategories.includes(category))
       : categoryList;
   }),
-  currentCategory: computed(function() {
-    return this.get('isCompleteList') ? 'all' : 'people' ;
-  }),
-  emojiList: computed('currentCategory',function() {
-    return (this.get('isCompleteList'))
-      ? this.get('emojis')
-      : this.get('emojis')[this.get('currentCategory')];
+  emojiList: computed('searchResults', 'currentListType',function() {
+    return (this.get('searchEnabled') && this.get('isSearchResult'))
+      ? this.get('searchResults')
+      : (this.get('isCompleteList'))
+        ? this.get('emojis')
+        : this.get('emojis')[this.get('currentListType')];
   }),
 
+  // Actions
   actions: {
+    searchEmojis() {
+      const searchTerm = this.get('searchTerm');
+      if (searchTerm.trim() === "") return;
+      const searchResults = this.get('emojidex').searchEmojis(searchTerm);
+      if(searchResults) {
+        this.set('searchResults', searchResults);
+      } else {
+        this.set('searchResults', 'none');
+      }
+    },
     changeCategory(category) {
-      this.set('currentCategory', category);
+      this.set('currentListType', category);
     },
     selectEmoji(emoji) {
       this.set('selectedEmoji', emoji);
--- addon/utils/emojidex.js
+++ addon/utils/emojidex.js
@@ -1,9 +1,12 @@
-import { emojidata } from './emojidata';
+//import { emojidata } from './emojidata';
 import { emojilist } from './emojilist';
 
 export const emojidex = {
-  emojidata: emojidata,
+  //emojidata: emojidata,
   emojilist: emojilist,
+  // _searchInSubcategory(searchTerm, emoji) {
+  //   return emoji.subcategory.split(/[- ]/).filter( word => word.length > 1).includes(searchTerm);
+  // },
   getCategoryNames() {
     return {
       people: "Smileys & People",
@@ -15,5 +18,20 @@ export const emojidex = {
       symbol: "Symbols",
       flag: "Flags"
     }
-  }
+  },
+  // searchEmojis(searchTerm) {
+  //   let searchResults = [];
+  //   for(let category in emojidata) {
+  //     let emojis = emojidata[category];
+  //     searchResults.push(
+  //       emojis.filter((emoji) => {
+  //           return (emoji.keyTerms && emoji.keyTerms.includes(searchTerm))
+  //           ||  (emoji.subcategory && this._searchInSubcategory(searchTerm, emoji))
+  //       }).map((emoji) => {
+  //         return emoji.char;
+  //       })
+  //     );
+  //   }
+  //   return searchResults.flat();
+  // }
 }
